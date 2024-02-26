97225c9e056268722a7e20279e0a44c17d8cf344
Krzysztof Hypki <khypki@pc15.home>
2024-02-26 14:39:12 +0100

    =Add button component

--- src/components/base/VButton.vue
+++ src/components/base/VButton.vue
@@ -1,9 +1,56 @@
 <template>
-  <button class="bg-indigo-700">
+  <component
+    :is="to ? 'RouterLink' : 'button'"
+    :to="to"
+    class="block rounded transition-all duration-300 disabled:opacity-75"
+    :class="[variantClassList, sizeClassList]"
+    :disabled="disabled"
+  >
     <slot />
-  </button>
+  </component>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type RouteLocationRaw } from 'vue-router'
+
+interface Props {
+  variant?: 'primary' | 'secondary'
+  size?: 'sm' | 'md' | 'lg'
+  disabled?: boolean
+  to?: RouteLocationRaw
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'primary',
+  size: 'md',
+  disabled: false
+})
+
+const variantClassList = computed(() => {
+  switch (props.variant) {
+    case 'primary':
+      return 'bg-indigo-700 text-white enabled:hover:bg-white enabled:hover:text-indigo-700'
+    case 'secondary':
+      return 'border-2 border-indigo-700 text-indigo-500 enabled:hover:bg-indigo-700 enabled:hover:text-white'
+    default:
+      return ''
+  }
+})
+
+const sizeClassList = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'px-2 py-1'
+    case 'md':
+      return 'px-2 py-1.5'
+    case 'lg':
+      return 'p-2.5'
+
+    default:
+      return ''
+  }
+})
+</script>
 
 <style scoped></style>
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    <div class="flex"></div>
     <VButton>test</VButton>
+    <VButton variant="secondary">test</VButton>
+    <VButton variant="secondary" size="lg">test</VButton>
+    <VButton variant="secondary">test</VButton>
+    <VButton variant="secondary" disabled>test</VButton>
+    <VButton to="/" variant="secondary" disabled>test</VButton>
   </div>
 </template>
 
